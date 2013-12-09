6b81d4594bd1170838fa2e66367977186f4fafa1
finalfantasyfreak15 <rollingstar.15@gmail.com>
2013-12-08 19:10:42 -0700

    finished login feature. now working on student queue activity features

--- TAQueue/src/main/java/utah/edu/cs4962/TAQueue/QueueLoginActivity.java
+++ TAQueue/src/main/java/utah/edu/cs4962/TAQueue/QueueLoginActivity.java
@@ -1,17 +1,11 @@
 package utah.edu.cs4962.TAQueue;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.InputType;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,7 +13,6 @@ import android.widget.TextView;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -33,10 +26,11 @@ public class QueueLoginActivity extends Activity
     private String _classNumber;
     private String _instructorUsername;
     private String _className;
-    private boolean isTA = false;
+    private boolean _isTA = false;
     private String _id;
     private String _token;
     private TextView _errorTextView;
+    private Context _context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -44,12 +38,15 @@ public class QueueLoginActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.queue_login);
 
-        _errorTextView = (TextView)findViewById(R.id.error_banner);
+        _context = this;
+
+        _errorTextView = (TextView) findViewById(R.id.error_banner);
 
         _client = QueueClientFactory.getInstance();
 
         Intent intent = getIntent();
 
+        //grab the info from the previous activity so we can build the url from it
         _schoolAbbrev = intent.getStringExtra("school_abbrev");
         _classNumber = intent.getStringExtra("class_number");
         _className = intent.getStringExtra("class_name");
@@ -60,17 +57,18 @@ public class QueueLoginActivity extends Activity
 
         final EditText usernameEditText = (EditText) findViewById(R.id.username_edittext);
         final EditText pwdOrLocEditText = (EditText) findViewById(R.id.pwd_or_loc_edittext);
+        final TextView pwdOrLocTextView = (TextView) findViewById(R.id.pwd_or_loc_textview);
 
-        final TextView pwdOrLocTextView = (TextView)findViewById(R.id.pwd_or_loc_textview);
-
-        Button studentButton = (Button)findViewById(R.id.student_button);
+        Button studentButton = (Button) findViewById(R.id.student_button);
         studentButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
                 pwdOrLocTextView.setText("Location");
-                isTA = false;
+                //show the location text
+                pwdOrLocEditText.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                _isTA = false;
             }
         });
 
@@ -81,105 +79,129 @@ public class QueueLoginActivity extends Activity
             public void onClick(View v)
             {
                 pwdOrLocTextView.setText("Password");
-                isTA = true;
+                //hide the password
+                pwdOrLocEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                _isTA = true;
             }
         });
 
 
-        Button loginButton = (Button)findViewById(R.id.login_button);
+        Button loginButton = (Button) findViewById(R.id.login_button);
         loginButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                if(usernameEditText.getText().length() != 0 && pwdOrLocEditText.getText().length() != 0)
+                //only let the user login if they have specified both the username and password fields.
+                if (usernameEditText.getText().length() != 0 && pwdOrLocEditText.getText().length() != 0)
                 {
                     loginUser(usernameEditText.getText().toString(), pwdOrLocEditText.getText().toString());
+                }
+                else
+                {
+                    if(_isTA)
+                    {
+                        _errorTextView.setText("Fill in username and password");
+                    }
+                    else
+                    {
+                        _errorTextView.setText("Fill in username and location");
+                    }
                 }
             }
         });
 
     }
 
+    /**
+     * Logs in the user student/TA into the selected queue
+     * @param username
+     * @param pwdOrLoc
+     */
     private void loginUser(String username, String pwdOrLoc)
     {
+        //the end part of the url which determines the type of user signing in.
         String studentOrTA;
+        //params to send with the HTTP request
         RequestParams params = new RequestParams();
-        JSONObject jsonCredentialsObject = new JSONObject();
-        JSONObject jsonUserObject = new JSONObject();
-        try
+
+        //keep copies of the method params to use in the response handler
+        final String _username = username;
+        final String _location = pwdOrLoc;
+
+        //format the request params based on type of user
+        if (_isTA)
         {
-            jsonCredentialsObject.put("username", username);
-
-            if(isTA)
-            {
-                studentOrTA = "/tas";
-                jsonCredentialsObject.put("password", pwdOrLoc);
-                jsonUserObject.put("ta", jsonCredentialsObject);
-                params.add("Authentication:", pwdOrLoc);
-            }
-            else
-            {
-                studentOrTA = "/students";
-                jsonCredentialsObject.put("location", pwdOrLoc);
-                jsonUserObject.put("student", jsonCredentialsObject);
-                params.add("Authentication:", "None");
-            }
-
-            String url = "schools/" + _schoolAbbrev + "/" + _instructorUsername + "/" + _classNumber + studentOrTA;
-
-//            params
-            params.add("Parameters:", jsonUserObject.toString());
-
-            _client.post(url, params, new JsonHttpResponseHandler()
-            {
-                @Override
-                public void onSuccess(JSONObject response)
-                {
-                    try
-                    {
-                        _id = response.getString("id");
-                        _token = response.getString("token");
-                    } catch (JSONException e)
-                    {
-                        e.printStackTrace();
-                    }
-
-                    Log.d("logged in successfully", "suceessfull");
-                }
-
-                @Override
-                public  void onFailure(int statusCode, Throwable e, JSONObject errorResponse)
-                {
-                    String errorMsg = null;
-                    try
-                    {
-                        errorMsg = errorResponse.getString("errors");
-                    } catch (JSONException e1)
-                    {
-                        e1.printStackTrace();
-                    }
-                    _errorTextView.setText(errorMsg);
-                }
-            });
-
-            if(_id != null && _token != null)
-            {
-                Intent queueActivity = new Intent(this, QueueActivity.class);
-                queueActivity.putExtra("id", _id);
-                queueActivity.putExtra("token", _token);
-                queueActivity.putExtra("isTA", isTA);
-                queueActivity.putExtra("class_number", _classNumber);
-                queueActivity.putExtra("instructor_username", _instructorUsername);
-                queueActivity.putExtra("school_abbrev", _schoolAbbrev);
-                this.startActivity(queueActivity);
-            }
-        } catch (JSONException e)
+            studentOrTA = "/tas.json";
+            params.add("ta[username]", username);
+            params.add("ta[password]", pwdOrLoc);
+        } else
         {
-            e.printStackTrace();
+            studentOrTA = "/students.json";
+            params.add("student[username]", username);
+            params.add("student[location]", pwdOrLoc);
         }
 
+        //build the url for the POST request that creates the new user and logs them into the queue.
+        String url = "schools/" + _schoolAbbrev + "/" + _instructorUsername + "/" + _classNumber + studentOrTA;
 
+        _client.post(url, params, new JsonHttpResponseHandler()
+        {
+            /**
+             * Successful user login request
+             * @param response
+             */
+            @Override
+            public void onSuccess(JSONObject response)
+            {
+                try
+                {
+                    //get the id and token returned
+                    _id = response.getString("id");
+                    _token = response.getString("token");
+
+                    //go to the TA queue activity
+                    if(_isTA)
+                    {
+                        Intent queueActivity = new Intent(_context, TAQueueActivity.class);
+                        queueActivity.putExtra("id", _id);
+                        queueActivity.putExtra("token", _token);
+                        queueActivity.putExtra("class_number", _classNumber);
+                        queueActivity.putExtra("instructor_username", _instructorUsername);
+                        queueActivity.putExtra("school_abbrev", _schoolAbbrev);
+                        queueActivity.putExtra("username", _username);
+                        _context.startActivity(queueActivity);
+                    }
+                    else //go to the student activity
+                    {
+                        Intent queueActivity = new Intent(_context, StudentQueueActivity.class);
+                        queueActivity.putExtra("id", _id);
+                        queueActivity.putExtra("token", _token);
+                        queueActivity.putExtra("class_number", _classNumber);
+                        queueActivity.putExtra("instructor_username", _instructorUsername);
+                        queueActivity.putExtra("school_abbrev", _schoolAbbrev);
+                        queueActivity.putExtra("username", _username);
+                        queueActivity.putExtra("location", _location);
+                        _context.startActivity(queueActivity);
+                    }
+                } catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+
+            /**
+             * What is displayed upon failure of the user login request
+             * @param statusCode
+             * @param e
+             * @param errorResponse
+             */
+            @Override
+            public void onFailure(int statusCode, Throwable e, JSONObject errorResponse)
+            {
+                _errorTextView.setText("Invalid password");
+            }
+        });
     }
 
 
